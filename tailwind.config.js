83653f05dc67193345ac54291c1f1f8151151a56
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-10-20 16:59:58 +0700

    feat: declare color pallete aplication

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-0": "#FFFFFF",
+        "primary-100": "#FAFAFA",
+        "primary-200": "#F7F7F7",
+        "primary-300": "#F3F3F3",
+        "secondary-200": "#66737E",
+        "error-100": "#E37E5D",
+        "error-200": "#D2524C",
+        success: "#E0F19B",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
