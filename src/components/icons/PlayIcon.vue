749b4ab2a4aae0ed9bd0559fdfe25df4d5a154f3
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-10-23 21:36:47 +0700

    feat: add new icons: donwload, error, play, trash, stop

---
+++ src/components/icons/PlayIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M21.6667 12L1.66666 1.33334V22.6667L21.6667 12Z"
+      stroke="#555555"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
