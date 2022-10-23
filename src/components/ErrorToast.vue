0e5485abad1d30f0c42fec7d8b00e8b85c2041d1
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2022-10-23 21:36:00 +0700

    feat: adding error toast component

---
+++ src/components/ErrorToast.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ErrorIcon from "./icons/ErrorIcon.vue";
+
+defineProps({
+  isError: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div
+    class="absolute py-2 px-3 bg-red-500 rounded-sm left-10 flex items-center gap-4 transition-[top] duration-500"
+    :style="{
+      top: isError ? '24px' : '-40px',
+    }"
+  >
+    <ErrorIcon />
+    <p class="text-white">
+      <slot />
+    </p>
+  </div>
+</template>
